6da266cac97e0d83169cc5be311046c8e781c41e
Adam Kłos <aklos@rcl.gov.pl>
2025-03-03 12:10:41 +0100

    Logic moved to library project

--- Program.cs
+++ Program.cs
@@ -7,31 +7,6 @@ namespace WordParser
 {
     class Program
     {
-        public static List<string> VALID_STYLES_BOLD = new List<string>
-        {
-            "_IG_ - indeks górny", 
-            "_IG_K_ - indeks górny i kursywa", 
-            "_IG_P_ - indeks górny i pogrubienie", 
-            "_IG_P_K_ - indeks górny i pogrubienie kursywa", 
-            "_IIG_ - indeks górny indeksu górnego", 
-            "_IIG_P_ - indeks górny indeksu górnego i pogrubienie"
-        };
-        public static List<string> VALID_STYLES_SUPERSCRIPT = new List<string>
-        {
-            "_P_ - pogrubienie", 
-            "_IG_P_ - indeks górny i pogrubienie", 
-            "TYT(DZ)_PRZEDM - przedmiot regulacji tytułu lub działu", 
-            "_IG_P_K_ - indeks górny i pogrubienie kursywa", 
-            "OZN_RODZ_AKTU - tzn. ustawa lub rozporządzenie i organ wydający", 
-            "_IIG_P_ - indeks górny indeksu górnego i pogrubienie", 
-            "TYTUŁ_AKTU - przedmiot regulacji ustawy lub rozporządzenia", 
-            "ROZDZ(ODDZ)_PRZEDM - przedmiot regulacji rozdziału lub oddziału", 
-            "TYT_TABELI - tytuł tabeli", 
-            "NAZ_ORG_WYD - nazwa organu wydającego projektowany akt", 
-            "NAZ_ORG_W_POROZUMIENIU - nazwa organu w porozumieniu z którym akt jest wydawany", 
-            "OZN_ZAŁĄCZNIKA - wskazanie nr załącznika"
-        };
-
         static void Main(string[] args)
         {
             if (args.Length < 2)
@@ -66,7 +41,7 @@ namespace WordParser
                 using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(filePath, true))
                 {
                     wordDoc.CompressionOption = CompressionOption.Maximum;
-                    var legalAct = new LegalAct(wordDoc);
+                    var legalAct = new WordParserLibrary.LegalAct(wordDoc);
 
                     // Usuwanie komentarzy autora 'System'
                     legalAct.RemoveSystemComments();
